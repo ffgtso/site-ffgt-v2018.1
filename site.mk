d75dd6b2cf7d5d6ad7b79152c9aeb4796dd62546
wusel42 <wusel+src@uu.org>
2018-07-15 22:57:44 +0200

    First adoption to 2018.1/fastd

--- site.mk
+++ site.mk
@@ -1,76 +1,30 @@
 ##	gluon site.mk makefile example
 
+##	GLUON_FEATURES
+#		Specify Gluon features/packages to enable;
+#		Gluon will automatically enable a set of packages
+#		depending on the combination of features listed
+
+GLUON_FEATURES := \
+	autoupdater \
+	ebtables-filter-multicast \
+	ebtables-filter-ra-dhcp \
+	ebtables-limit-arp \
+	mesh-batman-adv-14 \
+	mesh-vpn-fastd \
+	radvd \
+	respondd \
+	status-page \
+	web-advanced \
+	web-wizard
+
 ##	GLUON_SITE_PACKAGES
-#		specify gluon/openwrt packages to include here
-#		The gluon-mesh-batman-adv-* package must come first because of the dependency resolution
+#		Specify additional Gluon/LEDE packages to include here;
+#		A minus sign may be prepended to remove a packages from the
+#		selection that would be enabled by default or due to the
+#		chosen feature flags
 
-GLUON_SITE_PACKAGES := \
-	gluon-mesh-batman-adv-14 \
-	gluon-node-info-ffgt \
-	gluon-respondd \
-	gluon-autoupdater \
-	ffgt-geolocate \
-	ffgt-siteselect \
-	ffgt-banner \
-	ffgt-setup-mode \
-	gluon-luci-wifi-config-ffgt \
-	gluon-luci-portconfig-ffgt \
-	gluon-config-mode-autoupdater-ffgt \
-	gluon-config-mode-contact-info \
-	gluon-config-mode-core \
-	gluon-config-mode-geo-location-ffgt \
-	gluon-config-mode-hostname-ffgt \
-	gluon-config-mode-mesh-vpn \
-	gluon-ebtables-filter-multicast \
-	gluon-ebtables-filter-ra-dhcp \
-	gluon-luci-admin \
-	gluon-luci-private-wifi \
-	ffrn-lowmem-patches \
-	gluon-next-node \
-	gluon-mesh-vpn-fastd \
-	gluon-radvd \
-	gluon-setup-mode \
-	gluon-status-page-ffgt \
-	gluon-ssid-changer \
-	gluon-ffgt-hacks \
-	kmod-l2tp kmod-l2tp-ip kmod-l2tp-eth ip-full \
-	haveged \
-	iptables \
-	iwinfo
-
-# 2017-11-26 left out for now:
-#	ffho-autoupdater-wifi-fallback \
-#	ffho-luci-autoupdater-wifi-fallback \
-
-
-USB_BASIC := \
-	kmod-usb-core \
-	kmod-usb2 \
-	kmod-usb-hid
-
-
-USB_NIC := \
-	kmod-usb-net \
-	kmod-usb-net-asix \
-	kmod-usb-net-rtl8150 \
-	kmod-usb-net-rtl8152 \
-	kmod-usb-net-dm9601-ether
-
-ifeq ($(GLUON_TARGET),x86-generic)
-	GLUON_SITE_PACKAGES += \
-		$(USB_BASIC) \
-		kmod-usb-ohci-pci \
-		resize2fs \
-		$(USB_NIC)
-endif
-
-ifeq ($(GLUON_TARGET),x86-64)
-	GLUON_SITE_PACKAGES += \
-		$(USB_BASIC) \
-		$(USB_NIC) \
-		resize2fs \
-		kmod-igb #APU2
-endif
+GLUON_SITE_PACKAGES := haveged iwinfo
 
 ##	DEFAULT_GLUON_RELEASE
 #		version string to use for images
@@ -78,8 +32,9 @@ endif
 #			opkg compare-versions "$1" '>>' "$2"
 #		to decide if a version is newer or not.
 
-DEFAULT_GLUON_RELEASE := 0.6+exp$(shell date '+%Y%m%d')
+DEFAULT_GLUON_RELEASE := 1.0+exp$(shell date '+%Y%m%d')
 
+# Variables set with ?= can be overwritten from the command line
 
 ##	GLUON_RELEASE
 #		call make with custom GLUON_RELEASE flag, to use your own release version scheme.
@@ -88,7 +43,6 @@ DEFAULT_GLUON_RELEASE := 0.6+exp$(shell date '+%Y%m%d')
 #		would generate images named like this:
 #			gluon-ff%site_code%-23.42+5-%router_model%.bin
 
-# Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
 
 # Default priority for updates.
@@ -99,4 +53,3 @@ GLUON_REGION ?= eu
 
 # Languages to include
 GLUON_LANGS ?= en de
-GLUON_ATH10K_MESH ?= 11s
